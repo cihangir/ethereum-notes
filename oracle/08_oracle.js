f91a938acd59800f12d88ad2f2f3c6f89dd55df5
Cihangir SAVAS <cihangir@savas.io>
2017-07-11 17:06:22 -0700

    oracle: add driver file for oracle

--- oracle/08_oracle.js
+++ oracle/08_oracle.js
@@ -1,24 +1,23 @@
-var EthUtil = require("ethereumjs-util")
-var Web3 = require("web3")
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
-var solc = require("solc")
-var fs = require("fs")
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var solc = require('solc');
+var fs = require('fs');
+var abi = require('ethereumjs-abi')
 
 function getContract(name, cname, modifier) {
-
-  var src = fs.readFileSync(__dirname + "/" + name.toLowerCase() + ".sol", 'utf8')
+  var src = fs.readFileSync(__dirname + '/' + name.toLowerCase() + '.sol', 'utf8')
   if (modifier) {
-    src = modifier(src)
+    src = modifier(src);
   }
 
-  var compiled = solc.compile(src)
-  var ctr = compiled.contracts[":" + cname + ""]
-  var abiRaw = ctr.interface
-  var abi = JSON.parse(abiRaw)
+  var compiled = solc.compile(src);
+  if (!compiled.contracts) { throw new Error("compilation has errors", compiled); }
+
+  var ctr = compiled.contracts[':' + cname];
   return {
-    contract: web3.eth.contract(abi),
+    contract: web3.eth.contract(JSON.parse(ctr.interface)),
     bytecode: ctr.bytecode,
-    data: contractData = {
+    data: {
       from: web3.eth.accounts[0],
       data: ctr.bytecode,
       gasPrice: 22000000000,
@@ -27,67 +26,100 @@ function getContract(name, cname, modifier) {
   }
 }
 
-var dispatch = getContract("dispatch", "TestOracleDispatch")
+var dispatch = getContract('dispatch', 'TiteOracleDispatch');
 
 // first deploy dispatch
 var deployedDispatch = dispatch.contract.new(dispatch.data, function (err, dispatchContract) {
   if (!dispatchContract.address) { return } // if the address is not available yet, cb will be called more than once.
-  if (err) {
-    console.log("dispatchContract err:", err);
-    return
-  }
+  if (err) { throw new Error('dispatchContract err:', err); }
 
   console.log('dispatchContract address: ' + dispatchContract.address)
 
-  var lookup = getContract("Lookup", "TestOracleLookup")
+  var lookup = getContract('Lookup', 'TiteOracleLookup')
   var deployedLookup = lookup.contract.new(lookup.data, function (err, lookupContract) {
     if (!lookupContract.address) { return } // if the address is not available yet, cb will be called more than once.
-    if (err) {
-      console.log("lookupContract err:", err);
-      return
-    }
+    if (err) { throw new Error('lookupContract err:', err); }
 
     console.log('lookupContract address: ' + lookupContract.address)
 
     deployedLookup.setQueryAddress(dispatchContract.address, { from: web3.eth.accounts[0] }, function (err, resp) {
       if (err) {
-        console.log("setQueryAddress err:", err);
+        console.log('setQueryAddress err:', err);
         return
       }
 
-      deployedLookup.setResponseAddress(web3.eth.accounts[0], { from: web3.eth.accounts[0] }, function (err, resp) {
-        if (err) {
-          console.log("setQueryAddress err:", err);
-          return
+      deployedLookup.getQueryAddress({ from: web3.eth.accounts[0] }, function (err, resp) {
+        if (err) { throw new Error('getQueryAddress err:', err); }
+        if (resp != dispatchContract.address) {
+          throw new Error("dispatchContract.address is not same with the gotten one", dispatchContract.address, resp)
+        } else {
+          throw new Error("dispatchContract.address is same with the gotten one")
         }
+      })
 
-        console.log("resp from setResponseAddress", resp)
+      deployedLookup.setResponseAddress(web3.eth.accounts[0], { from: web3.eth.accounts[0] }, function (err, resp) {
+        if (err) { throw new Error('setQueryAddress err:', err); }
+
+        deployedLookup.getResponseAddress({ from: web3.eth.accounts[0] }, function (err, resp) {
+          if (err) { throw new Error('getResponseAddress err:', err); }
+
+          if (resp != web3.eth.accounts[0]) {
+            throw new Error("web3.eth.accounts[0] is not same with the gotten one", web3.eth.accounts[0], resp)
+          }
+        })
+
+        console.log('resp from setResponseAddress', resp)
 
         var client = getContract("Client", "SampleClient", function (s) { return s.replace("0x0", lookupContract.address) })
         var deployedClient = client.contract.new(client.data, function (err, clientContract) {
           if (!clientContract.address) { return } // if the address is not available yet, cb will be called more than once.
-          if (err) {
-            console.log("clientContract err:", err);
-            return
-          }
+          if (err) { throw new Error('clientContract err:', err); }
 
           console.log('clientContract address: ' + clientContract.address)
+          console.log("lookupContract.address", lookupContract.address)
 
-          var f = function(){
-            deployedClient.query.call(function (err, resp) {
-              if (err) {
-                console.log("deployedClient.query err:", err);
-                return
-              }
-              console.log("resp", resp.valueOf())
-
+          var count = 1
+          var send = function () {
+            deployedClient.querySend("hello world" + count, { from: web3.eth.accounts[0] }, function (err, resp) {
+              if (err) { throw new Error('deployedClient.search err:', err); }
+              count++
+              deployedClient.response.call({ from: web3.eth.accounts[0] }, function (err, resp) {
+                if (err) { throw new Error('deployedClient.getResponse err:', err); }
+                console.log('getResponse', resp.valueOf())
+              })
             })
           }
-          setInterval(f, 1000);
-          
-            setTimeout(function () {
-              console.log('Blah blah blah blah extra-blah');
-            }, 30000);
+
+
+          var filter = web3.eth.filter({ fromBlock: 0, toBlock: 'latest', address: dispatchContract.address }, (function (err, event) {
+            if (err) { throw new Error("web3.eth.filter err:", err); }
+            var data = abi.rawDecode(['uint256', 'address', 'string'], new Buffer(event.data.replace("0x", ""), 'hex'));
+            var uuid = data[0]
+            var sender = "0x" + data[1].toString('hex')
+            var request = data[2] // do something with the request.
+
+            var a = abi.methodID('__titeOracleCallback', ['uint256', 'string']).toString('hex')
+            var b = abi.rawEncode(['uint256', 'string'], [uuid, request]).toString('hex')
+
+            var tx = {
+              from: web3.eth.accounts[0],
+              to: sender,
+              data: a + b
+            };
+
+            web3.eth.sendTransaction(tx, function (err, txHash) {
+              if (err) { throw new Error("sendTransaction err:", err); }
+            })
+          }));
+
+          setInterval(send, 100);
+          send();
+
+          setTimeout(function () {
+            console.log('Blah blah blah blah extra-blah');
+            filter.stopWatching();
+            process.exit(0)
+          }, 30000);
         })
       })
     })
